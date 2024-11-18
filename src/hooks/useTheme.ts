687f79d9f274eb9bee96b53b900a77ce974749c9
Sid <sidvermas1234@gmail.com>
2024-11-18 10:18:22 +0530

    useTheme with component

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootStateI, useAppDispatch } from '../store/store'
-import { toggleTheme } from '../store/reducers/settingsSlice'
+import {  toggleTheme, } from '../store/reducers/settingsSlice'
+import { ThemesI } from '../types/store/settings'
 
 const useTheme = () => {
   const dispatch = useAppDispatch()
   const { theme } = useSelector((state: RootStateI) => state.settings)
   const updateTheme = React.useCallback(() => {
     dispatch(toggleTheme())
-    console.log(theme);
-    
-    document.body.classList.toggle(theme)
+    document.body.classList.toggle('dark' as ThemesI)
   }, [dispatch, theme])
   return { theme, updateTheme }
 }
